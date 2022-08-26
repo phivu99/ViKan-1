fe24da7837034ae424be0504b31c0e87a0b9502b
phivu99 <lephivu99@gmail.com>
2022-08-26 17:05:58 +0700

    up

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -311,4 +311,33 @@ var swiper =  new Swiper(".carousel-9", {
     },
 });
 
+var swiper =  new Swiper(".carousel-team", {
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    //     },
+    loop:false,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        clickable: true,
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
 
